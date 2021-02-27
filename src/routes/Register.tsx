8e3cfe7a0ba7659dab02d41b35dce7ee73755669
ExtraHash <ExtraHash@protonmail.com>
2021-02-27 16:49:09 -0700

    update login pages

--- src/routes/Register.tsx
+++ src/routes/Register.tsx
@@ -34,65 +34,61 @@ function Register(props: {
     };
 
     return (
-        <div class="aligner">
-            <div class="aligner-item aligner-item--top" />
-            <div class="card aligner-item">
-                <div class="box container pinched">
-                    <h5 class="centered">Welcome to miniwallet</h5>
-                    <form
-                        onSubmit={(event): void => {
-                            event.preventDefault();
-                            register();
+        <div class="card container no-menu">
+            <div class="box">
+                <h5 class="centered">Welcome to miniwallet</h5>
+                <form
+                    onSubmit={(event): void => {
+                        event.preventDefault();
+                        register();
+                    }}
+                >
+                    <label>Username:</label>
+                    <input
+                        type="text"
+                        value={username}
+                        onInput={(event: any): void => {
+                            setUsername(event.target.value);
                         }}
-                    >
-                        <label>Username:</label>
-                        <input
-                            type="text"
-                            value={username}
-                            onInput={(event: any): void => {
-                                setUsername(event.target.value);
-                            }}
-                        />
-                        <div class="row">
-                            <div class="six columns">
-                                <label>Password: </label>
-                                <input
-                                    type="password"
-                                    value={password}
-                                    onInput={(event: any): void => {
-                                        setPassword(event.target.value);
-                                    }}
-                                />
-                            </div>
-                            <div class="six columns">
-                                <label>Confirm Password: </label>
-                                <input
-                                    type="password"
-                                    value={confirm}
-                                    onInput={(event: any): void => {
-                                        setConfirm(event.target.value);
-                                    }}
-                                />
-                            </div>
-                            <div class="buttons right">
-                                <button class="button-primary">Register</button>
-                            </div>
-                            <div class="buttons">
-                                <button
-                                    type="submit"
-                                    class="button-ghost"
-                                    onClick={(): void => {
-                                        route("/login");
-                                    }}
-                                >
-                                    Sign in instead
-                                </button>
-                            </div>
+                    />
+                    <div class="row">
+                        <div class="six columns">
+                            <label>Password: </label>
+                            <input
+                                type="password"
+                                value={password}
+                                onInput={(event: any): void => {
+                                    setPassword(event.target.value);
+                                }}
+                            />
                         </div>
-                    </form>
-                </div>
+                        <div class="six columns">
+                            <label>Confirm Password: </label>
+                            <input
+                                type="password"
+                                value={confirm}
+                                onInput={(event: any): void => {
+                                    setConfirm(event.target.value);
+                                }}
+                            />
+                        </div>
+                        <div class="buttons right">
+                            <button class="button-primary">Register</button>
+                        </div>
+                        <div class="buttons">
+                            <button
+                                type="submit"
+                                class="button-ghost"
+                                onClick={(): void => {
+                                    route("/login");
+                                }}
+                            >
+                                Sign in instead
+                            </button>
+                        </div>
+                    </div>
+                </form>
             </div>
-            <div class="aligner-item aligner-item--bottom" />
         </div>
     );
 }
